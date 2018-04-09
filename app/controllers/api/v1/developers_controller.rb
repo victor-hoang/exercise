a8011b64312203e8adf39005585cdb681f4b917a
victor hoang <lim.hoang@tech.reapra.sg>
2018-04-10 04:16:46 +0700

    Add api controller

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -3,14 +3,23 @@
 module Api
   module V1
     class DevelopersController < Api::BaseController
-      def index
-        developers = Developer.includes(
-          :developer_programming_languages,
-          :programming_languages,
-          :developer_languages,
-          :languages
-        )
-        render json: developers, include: []
+      before_action :set_developer, only: %i[show]
+
+      def show
+        # rubocop:disable Style/GuardClause
+        if stale?(last_modified: @developer.updated_at.utc)
+          cache_options = { compress: true, expires_in: 24.hours, race_condition_ttl: 10 }
+          Rails.cache.fetch("#{@developer.cache_key}/#{__method__}", cache_options) do
+            render json: @developer.to_json(include: %i[programming_languages languages])
+          end
+        end
+        # rubocop:enable Style/GuardClause
+      end
+
+      private
+
+      def set_developer
+        @developer = Developer.find(params[:id])
       end
     end
   end
